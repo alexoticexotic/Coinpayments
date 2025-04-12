31e6a896c22888895680373bedd7654c5c61a36d
Your Name <alexandro-97@inbox.lv>
2025-04-12 15:21:01 +0300

    Fix IPN_SECRET to match CoinPayments

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 
 TELEGRAM_BOT_TOKEN = '8129372019:AAFP33Rzl6taohTdpmogME-DZETwK3RV1Nc'
 TELEGRAM_CHAT_ID = '226453434'
-IPN_SECRET = 'МояЛюбимаяКошечкаК'  # тот самый, что у тебя в CoinPayments
+IPN_SECRET = 'МояЛюбимаяКошечкаКитти'  # тот самый, что у тебя в CoinPayments
 
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
